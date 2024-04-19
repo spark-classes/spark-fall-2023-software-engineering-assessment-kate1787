77651a876de9296aee3002071693d03c1382516a
김민경 <kimminkyung@crc-dot1x-nat-10-239-217-113.bu.edu>
2024-04-18 21:34:42 -0400

    Fill globals.ts

--- src/globals.ts
+++ src/globals.ts
@@ -3,14 +3,14 @@
  * We've put a few here that we know you will need.
  * Fill in the blank for each one
  */
-export const MY_BU_ID = "";
+export const MY_BU_ID = "U11586865";  
 export const BASE_API_URL = "https://spark-se-assessment-api.azurewebsites.net/api";
-// You can get this from Gradescope aka x-functions-key
-export const TOKEN = "";
-// This is a helper function to generate the headers with the x-functions-key attached
+export const TOKEN = "6se7z2q8WGtkxBlXp_YpU-oPq53Av-y_GSYiKyS_COn6AzFuTjj4BQ==";
+
+
 export const GET_DEFAULT_HEADERS = () => {
   var headers = new Headers();
-  // You will need to add another header here
-  // If you do not, the API will reject your request (:
+  headers.append('x-functions-key', TOKEN);
+  headers.append('BU-ID', MY_BU_ID); // Added your BU ID to the default headers
   return headers;
 };
